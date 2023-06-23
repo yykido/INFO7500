76b9c28b371193a80b8aea86585a055e15ad88da
yystephan <15990767462@163.com>
2023-06-23 18:46:35 +0800

    basically finish v4.0

---
+++ v4.0/contracts/NFTDutchAuction_ERC20Bids.sol
@@ -0,0 +1,92 @@
+// contracts/MyNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+
+contract NFTDutchAuction_ERC20Bids is ERC721PresetMinterPauserAutoId {
+    address payable public seller;
+    uint256 public reservePrice;
+    uint256 public numBlocksAuctionOpen;
+    uint256 public offerPriceDecrement;
+    uint256 public initialPrice;
+    uint256 public auctionEndTime;
+    bool public auctionEnded;
+    address payable public firstBidder;
+    uint256 public firstBid;
+    bool public gotValidBid;
+    address public tokenAddress;
+    uint256 public nftTokenId;
+    address public erc20TokenAddress;
+
+    mapping(address => uint256) public bids;
+
+    event AuctionEnded(address winner, uint256 amount);
+
+    constructor(
+    address _erc20TokenAddress, 
+    address erc721TokenAddress, 
+    uint256 _nftTokenId, 
+    uint256 _reservePrice, 
+    uint256 _numBlocksAuctionOpen, 
+    uint256 _offerPriceDecrement)
+    ERC721PresetMinterPauserAutoId("MyNFT", "MNFT", "https://northeastern.edu/nft") {
+        seller = payable(msg.sender);
+        reservePrice = _reservePrice;
+        numBlocksAuctionOpen = _numBlocksAuctionOpen;
+        offerPriceDecrement = _offerPriceDecrement;
+        initialPrice = reservePrice + numBlocksAuctionOpen * offerPriceDecrement;
+        auctionEndTime = block.number + numBlocksAuctionOpen;
+        auctionEnded = false;
+        gotValidBid = false;
+        firstBidder = payable(address(0));
+        firstBid = 0;
+        tokenAddress = erc721TokenAddress;
+        nftTokenId = _nftTokenId;
+        erc20TokenAddress = _erc20TokenAddress;
+    }
+
+    function bid(uint256 amount) public returns (bool) {
+        uint256 currentPrice = getCurrentPrice();
+        require(currentPrice > 0, "Auction has ended");
+        require(amount > 0, "Bid amount must be greater than 0");
+        require(amount >= currentPrice, "Bid amount is lower than current price");
+
+        if (!gotValidBid) {
+            firstBidder = payable(msg.sender);
+            firstBid = amount;
+            gotValidBid = true;
+            bids[seller] += firstBid;
+            IERC20(erc20TokenAddress).transferFrom(msg.sender, seller, firstBid);
+            return true;
+        }
+        bids[msg.sender] += amount;
+        IERC20(erc20TokenAddress).transferFrom(msg.sender, seller, amount);
+        return false;
+    }
+
+    function getCurrentPrice() public view returns (uint256) {
+        if (block.number >= auctionEndTime) {
+            return 0; // Auction has ended
+        } else {
+            uint256 blocksRemaining = auctionEndTime - block.number;
+            return initialPrice - blocksRemaining * offerPriceDecrement;
+        }
+    }
+
+    function claimRefund() external {
+        require(bids[msg.sender] > 0, "No refund available");
+
+        uint256 refundAmount = bids[msg.sender];
+        bids[msg.sender] = 0;
+        console.log('refundAmount: ');
+        console.log(refundAmount);
+        IERC20(erc20TokenAddress).transferFrom(seller, msg.sender, refundAmount);
+        // IERC20(erc20TokenAddress).transfer(msg.sender, refundAmount);
+    }
+    function getBalanceOf(address account) public view returns (uint256) {
+        return bids[account];
+    }
+}
--- v4.0/node_modules/hardhat/sample-projects/javascript/scripts/deploy.js
+++ v4.0/node_modules/hardhat/sample-projects/javascript/scripts/deploy.js
@@ -10,18 +10,17 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.waitForDeployment();
+  await lock.deployed();
 
   console.log(
-    `Lock with ${ethers.formatEther(
+    `Lock with ${ethers.utils.formatEther(
       lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
   );
 }
 
